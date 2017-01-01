5966423453e3389427110d9ff8e7e0219e38962f
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2017-01-01 22:39:49 +0530

    Update Encode.ps1

--- Encode.ps1
+++ Encode.ps1
@@ -1,3 +1,4 @@
+#A small code to convert plain text strings into BASE64 encoded string
 $command = Read-Host -Prompt 'Input String for Encoding'
 $bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
 $ec =[Convert]::ToBase64String($bytes)
